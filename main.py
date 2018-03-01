4f25ff39d07455c0e9d53c03770ecda1ba185fe9
Marco Favorito <marco.favorito@gmail.com>
2018-03-01 18:52:48 +0100

    Input

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import sys
 
+from src.Input import Input
+
+
 def main():
-    pass
+    filename = sys.argv[0]
+    with open(filename) as f:
+        input_obj = Input(*f.readline().split())
+
+
+
 
 if __name__ == '__main__':
     main()
---
+++ src/Input.py
@@ -0,0 +1,8 @@
+class Input(object):
+    def __init__(self, rows, columns, vehicles, rides, bonus, steps):
+        self.rows       = rows
+        self.columns     = columns
+        self.vehicles   = vehicles
+        self.rides      = rides
+        self.bonus      = bonus
+        self.steps      = steps
