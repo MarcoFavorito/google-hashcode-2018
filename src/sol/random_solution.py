3cb0b4d5fdeb0e53ebfac5ba1a9572480c16e6b7
Marco Favorito <marco.favorito@gmail.com>
2018-03-01 21:09:29 +0100

    fix on repo

--- src/sol/random_solution.py
+++ src/sol/random_solution.py
@@ -1,3 +1,16 @@
 from src.Output import Output
 from src.Input import Input
 import random
+
+def random_solution(input_obj: Input) -> Output:
+    vehicles2rides = {}
+    rides_num = list(range(len(input_obj.rides)))
+    random.shuffle(rides_num)
+    while rides_num:
+        for v in range(1, input_obj.vehicles+1):
+            if not rides_num:break
+            if v not in vehicles2rides:
+                vehicles2rides[v] = []
+            vehicles2rides[v].append(rides_num.pop())
+
+    return Output(vehicles2rides)
