88d46cae36376385200b1cc595caada29aa3e446
Marco Favorito <marco.favorito@gmail.com>
2018-03-01 20:57:39 +0100

    first version of new solution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 
+from src.sol import earliest_start_earliest_finish
 from src.sol.random_solution import random_solution
 from src.utils import parse_input
 
@@ -8,7 +9,8 @@ def main():
     filename = sys.argv[1]
     input_obj = parse_input(filename)
 
-    o = random_solution(input_obj)
+    # o = random_solution(input_obj)
+    o = earliest_start_earliest_finish.sol(input_obj)
     print(o)
 
 
