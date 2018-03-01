919f1a9d3ad766e8ad94611b6c4f50c7099aa776
Marco Favorito <marco.favorito@gmail.com>
2018-03-01 19:02:21 +0100

    Output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 from src.Input import Input
+from src.Output import Output
 
 
 def main():
@@ -8,6 +9,12 @@ def main():
     with open(filename) as f:
         input_obj = Input(*f.readline().split(" "))
     print(input_obj)
+
+
+    # example of output
+    v2r = {1: [0,1]}
+    o = Output(v2r)
+    print(o)
 
 
 
---
+++ src/Output.py
@@ -0,0 +1,7 @@
+class Output(object):
+    def __init__(self, vehicles2rides):
+        self.vehicles2rides = vehicles2rides
+
+
+    def __str__(self):
+        return "\n".join("this vehicleis assigned %s ride: %s" % (v, l) for v,l in self.vehicles2rides.items())
