7c22e9a52ba2d66bdc1d8d1f9c54e7f8fb05cbb8
jacopo <jack1395@hotmail.it>
2018-03-01 21:15:10 +0100

    vehicle

--- src/Vehicle.py
+++ src/Vehicle.py
@@ -1,6 +1,11 @@
 class Vehicle(object):
     def __init__(self, id):
         self.id = id
+        self.position= (0,0)
+
+    #position deve essere una lista di 2 interi (x,y)
+    def move(self, position):
+        self.position = position
 
 
     def __str__(self):
